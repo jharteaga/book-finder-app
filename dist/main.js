d8b0370386438d27d0a1f4c1c6316ced9baccb1e
jharteaga <jh.arteaga09@gmail.com>
2020-04-11 02:59:18 -0700

    Added consume Google Books API and show books

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,40 @@
 const input = document.getElementById('query');
+const form = document.getElementById('form');
+const results = document.querySelector('.main-container__results');
+
+//Call API to search book
+async function searchBook(book) {
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${book}`
+  );
+  const data = await response.json();
+  createBook(data.items);
+}
+
+//Add books into results container
+function createBook(books) {
+  let output = '';
+  books.forEach((book) => {
+    output += `
+        <div class="main-container__results__book">
+            <img
+            src="${book.volumeInfo.imageLinks.thumbnail}"
+            alt="book cover"
+            class="main-container__results__book__cover"
+            />
+        </div>`;
+  });
+
+  results.innerHTML = output;
+}
+
+//Event Listeners
+input.addEventListener('keydown', (e) => {
+  if ((e.keyCode === 13) & (input.value.trim() !== '')) {
+    searchBook(input.value);
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
